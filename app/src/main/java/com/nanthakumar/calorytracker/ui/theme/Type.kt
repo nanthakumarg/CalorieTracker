7a8f1aad4f4533534d68713d8a22a8cd84fd6940
Nantha Kumar G <nantha2011batch@gmail.com>
2022-12-26 10:54:18 +0530

    domain change

--- app/src/main/java/com/nanthakumar/calorytracker/ui/theme/Type.kt
+++ app/src/main/java/com/nanthakumar/calorytracker/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.plcoding.calorytrackerprep.ui.theme
+package com.nanthakumar.calorytrackerprep.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
--- app/src/test/java/com/nanthakumar/calorytracker/ExampleUnitTest.kt
+++ app/src/test/java/com/nanthakumar/calorytracker/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.plcoding.calorytracker
+package com.nanthakumar.calorytracker
 
 import org.junit.Test
 
