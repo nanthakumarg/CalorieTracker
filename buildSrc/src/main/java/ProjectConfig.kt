035807052fb742d723288d906477c7f0728e7c6e
Nantha Kumar G <nantha2011batch@gmail.com>
2022-12-27 14:48:18 +0530

    Welcome screen commit

---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,8 @@
+object ProjectConfig {
+    const val appId = "com.nanthakumar.calorytracker"
+    const val compileSdk = 33
+    const val minSdk = 21
+    const val targetSdk = 31
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
