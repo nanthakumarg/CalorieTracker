c6ba67aafb2901d0115542ff54a60fae03fb8328
Nantha Kumar G <nantha2011batch@gmail.com>
2022-12-28 16:28:49 +0530

    Gender Screen

--- app/src/main/java/com/nanthakumar/calorytracker/MainActivity.kt
+++ app/src/main/java/com/nanthakumar/calorytracker/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.navigation.compose.rememberNavController
 import com.nanthakumar.calorytracker.navigation.navigate
 import com.nanthakumar.calorytracker.ui.theme.CaloryTrackerTheme
 import com.nanthakumar.core.navigation.Route
+import com.nanthakumar.onboarding_presentation.gender.GenderScreen
 import com.nanthakumar.onboarding_presentation.welcome.WelcomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
                     }
                     composable(Route.GENDER) {
-
+                        GenderScreen(onNavigate = navController::navigate)
                     }
                     composable(Route.HEIGHT) {
 
